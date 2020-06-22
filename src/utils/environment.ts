6eed9fcf598e28bd440f7a43041de9fb70a67cf9
Goutham <66022350+goutham-murugesan@users.noreply.github.com>
2020-06-22 18:50:31 +0530

    Updated driver service endpoint

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://dveun65l0d.execute-api.ap-south-1.amazonaws.com';
-export const API_URL = `${BASE_URL}/Prod`;
+export const BASE_URL = 'https://7e4i6mjoul.execute-api.us-east-1.amazonaws.com';
+export const API_URL = `${BASE_URL}/dev`;
